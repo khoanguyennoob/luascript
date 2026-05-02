632d90562de62abe665f7a8253501477a36eb23a
khoanguyennoob <109150789+khoanguyennoob@users.noreply.github.com>
2026-05-02 21:32:30 +0700

    Refactor LexusNotify function and main loop logic

--- script.lua
+++ script.lua
@@ -7,25 +7,25 @@ _G.LexusLastScan = _G.LexusLastScan or 0
 local LastProcessedVehicle = nil
 
 _G.LexusNotify = function(msg)
-    pcall(function()
-        local s3, IngameTipsTools = pcall(require, "GameLua.Mod.BaseMod.Common.UI.InGameTipsTools")
-        if s3 and IngameTipsTools and IngameTipsTools.BattleNormalTips then
-            IngameTipsTools.BattleNormalTips("Lexusmod: " .. msg, 2, 3)
-        end
+    pcall(function()
+        local s3, IngameTipsTools = pcall(require, "GameLua.Mod.BaseMod.Common.UI.InGameTipsTools")
+        if s3 and IngameTipsTools and IngameTipsTools.BattleNormalTips then
+            IngameTipsTools.BattleNormalTips("Lexusmod: " .. msg, 2, 3)
+        end
 
-        local s, GameplayData = pcall(require, "GameLua.GameCore.Data.GameplayData")
-        if not s or not GameplayData then return end
-        local uPlayerController = GameplayData.GetPlayerController()
-        if not uPlayerController then return end
+        local s, GameplayData = pcall(require, "GameLua.GameCore.Data.GameplayData")
+        if not s or not GameplayData then return end
+        local uPlayerController = GameplayData.GetPlayerController()
+        if not uPlayerController then return end
 
-        local s2, STExtraBlueprintFunctionLibrary = pcall(import, "STExtraBlueprintFunctionLibrary")
-        if s2 and STExtraBlueprintFunctionLibrary then
-            local chatComp = STExtraBlueprintFunctionLibrary.GetChatComponentFromController(uPlayerController)
-            if chatComp and chatComp.AddMsgInClient then
-                chatComp:AddMsgInClient("<ChatQuickMsg>Lexusmod: " .. msg .. "</>")
-            end
-        end
-    end)
+        local s2, STExtraBlueprintFunctionLibrary = pcall(import, "STExtraBlueprintFunctionLibrary")
+        if s2 and STExtraBlueprintFunctionLibrary then
+            local chatComp = STExtraBlueprintFunctionLibrary.GetChatComponentFromController(uPlayerController)
+            if chatComp and chatComp.AddMsgInClient then
+                chatComp:AddMsgInClient("<ChatQuickMsg>Lexusmod: " .. msg .. "</>")
+            end
+        end
+    end)
 end
 
 -- ==========================================
@@ -33,128 +33,143 @@ end
 -- ==========================================
 
 local function LexusMainLoop()
-    local s, GameplayData = pcall(require, "GameLua.GameCore.Data.GameplayData")
-    if not s or not GameplayData then return end
-    local uPlayerController = GameplayData.GetPlayerController()
-    if not slua.isValid(uPlayerController) then return end
-    local uPlayerCharacter = uPlayerController:GetCurPawn()
-    if not slua.isValid(uPlayerCharacter) then return end
+    local s, GameplayData = pcall(require, "GameLua.GameCore.Data.GameplayData")
+    if not s or not GameplayData then return end
+    local uPlayerController = GameplayData.GetPlayerController()
+    if not slua.isValid(uPlayerController) then return end
+    local uPlayerCharacter = uPlayerController:GetCurPawn()
+    if not slua.isValid(uPlayerCharacter) then return end
 
-    local sMark, InGameMarkTools = pcall(require, "GameLua.Mod.BaseMod.Common.InGameMarkTools")
-    local UGameplayStatics = import("GameplayStatics")
-    local CharacterClass = import("/Script/Engine.Character")
+    local sMark, InGameMarkTools = pcall(require, "GameLua.Mod.BaseMod.Common.InGameMarkTools")
+    local UGameplayStatics = import("GameplayStatics")
+    local CharacterClass = import("/Script/Engine.Character")
 
-    local currentTime = os.clock()
+    local currentTime = os.clock()
 
-    -- [LUỒNG CHẬM] 2 Giây/lần: Quét địch, Súng, Xe
-    if currentTime - _G.LexusLastScan > 2.0 then
-        _G.LexusLastScan = currentTime
+    -- [LUỒNG CHẬM] 2 Giây/lần: Quét địch, Súng, Xe
+    if currentTime - _G.LexusLastScan > 2.0 then
+        _G.LexusLastScan = currentTime
 
-        -- 1. MOD SÚNG (Giảm giật)
-        if uPlayerCharacter.GetCurrentShootWeapon then
-            local uWeaponManager = uPlayerCharacter:GetWeaponManager()
-            if slua.isValid(uWeaponManager) and uWeaponManager.HideCurrentWeapon ~= true then
-                local CurrentWeapon = uPlayerCharacter:GetCurrentShootWeapon()
-                if slua.isValid(CurrentWeapon) then
-                    local shootComp = CurrentWeapon.ShootWeaponComponent
-                    local ShootEntity = CurrentWeapon.ShootWeaponEntity or (shootComp and shootComp.ShootWeaponEntityComponent)
-                    local ShootEffect = CurrentWeapon.ShootWeaponEffect or (shootComp and (shootComp.ShootWeaponEffectComp or shootComp.ShootWeaponEffectComponent))
+        -- 1. MOD SÚNG (Giảm giật)
+        if uPlayerCharacter.GetCurrentShootWeapon then
+            local uWeaponManager = uPlayerCharacter:GetWeaponManager()
+            if slua.isValid(uWeaponManager) and uWeaponManager.HideCurrentWeapon ~= true then
+                local CurrentWeapon = uPlayerCharacter:GetCurrentShootWeapon()
+                if slua.isValid(CurrentWeapon) then
+                    local shootComp = CurrentWeapon.ShootWeaponComponent
+                    local ShootEntity = CurrentWeapon.ShootWeaponEntity or (shootComp and shootComp.ShootWeaponEntityComponent)
+                    local ShootEffect = CurrentWeapon.ShootWeaponEffect or (shootComp and (shootComp.ShootWeaponEffectComponent or shootComp:GetShootWeaponEffectComponent()))
 
-                    if slua.isValid(ShootEntity) and slua.isValid(ShootEffect) then
-                        if ShootEntity.AccessoriesVRecoilFactor ~= 0.11 then
-                            ShootEntity.bRecordHitDetail = false
-                            ShootEntity.RecoilKickADS = 0.11
-                            ShootEntity.AccessoriesVRecoilFactor = 0.11
-                            ShootEntity.AccessoriesHRecoilFactor = 0.07
-                            ShootEntity.GameDeviationFactor = 0.0
-                            ShootEffect.CameraShakeInnerRadius = 0.0
-                        end
-                    end
-                end
-            end
-        end
+                    if slua.isValid(ShootEntity) and slua.isValid(ShootEffect) then
+                        if ShootEntity.AccessoriesVRecoilFactor ~= 0.11 then
+                            ShootEntity.bRecordHitDetail = false
+                            ShootEntity.RecoilKickADS = 0.11
+                            ShootEntity.AccessoriesVRecoilFactor = 0.11
+                            ShootEntity.AccessoriesHRecoilFactor = 0.07
+                            ShootEntity.GameDeviationFactor = 0.0
+                            ShootEffect.CameraShakeInnerRadius = 0.0
+                            if ShootEntity.AutoAimingConfig then
+                               local AutoAimingConfig = ShootEntity.AutoAimingConfig
+                               local InnerRange = AutoAimingConfig.InnerRange  
+                               local OuterRange = AutoAimingConfig.OuterRange  
+                               if InnerRange then
+                                   InnerRange.Speed = 8
+                                   InnerRange.SpeedRate = 5
+                                   InnerRange.SpeedRateSight = 3
+                                   InnerRange.DyingRate = 0.1
+                               end
+                               if OuterRange then
+                                   OuterRange.Speed = 8
+                                   OuterRange.SpeedRate = 5
+                                   OuterRange.SpeedRateSight = 3
+                                   OuterRange.DyingRate = 0.1
+                               end
 
-        -- 2. MOD XE (Xăng & Skin)
-        local playerVehicle = uPlayerCharacter:GetVehicleCommon()
-        if slua.isValid(playerVehicle) then
-            -- [A] Hồi xăng (Chỉ nạp khi xăng dưới 10%)
-            local currentFuel = playerVehicle.Fuel or 0
-            if currentFuel < 10 then
-                local FuelMax = playerVehicle:GetFuelMax()
-                playerVehicle:OnRep_Fuel(FuelMax)
-                playerVehicle:SetFuelMax(FuelMax, true)
-                LexusNotify("Đã nạp đầy xăng!")
-            end
+                            end
+                        end
+                    end
+                end
+            end
+        end
 
-            -- [B] Đổi Skin (Sử dụng API uPlayerController:GetVehicleAvatar())
-            if LastProcessedVehicle ~= playerVehicle then
-                LexusNotify("Đổi skin!")
-                local AvatarComp = nil
-                if type(uPlayerController.GetVehicleAvatar) == "function" then
-                    AvatarComp = uPlayerController:GetVehicleAvatar()
-                    LexusNotify("Nhận avtarvh")
-                end
+        -- 2. MOD XE (Xăng & Skin)
+        local playerVehicle = uPlayerCharacter:GetVehicleCommon()
+        if slua.isValid(playerVehicle) then
+            -- [A] Hồi xăng (Chỉ nạp khi xăng dưới 10%)
+            local currentFuel = playerVehicle.Fuel or 0
+            if currentFuel < 10 then
+                local FuelMax = playerVehicle:GetFuelMax()
+                playerVehicle:OnRep_Fuel(FuelMax)
+                playerVehicle:SetFuelMax(FuelMax, true)
+                LexusNotify("Đã nạp đầy xăng!")
+            end
 
-                if not slua.isValid(AvatarComp) then
-                    local VehicleCommon = playerVehicle:GetCommonComponent()
-                    if slua.isValid(VehicleCommon) and type(VehicleCommon.GetAvatarComponent) == "function" then
-                        AvatarComp = VehicleCommon:GetAvatarComponent()
-                    end
-                end
+            -- [B] Đổi Skin (Sử dụng API uPlayerController:GetVehicleAvatar())
+            if LastProcessedVehicle ~= playerVehicle then
+                local AvatarComp = nil
+                if type(uPlayerController.GetVehicleAvatar) == "function" then
+                    AvatarComp = uPlayerController:GetVehicleAvatar()
+                end
 
-                if slua.isValid(AvatarComp) and type(AvatarComp.ChangeItemAvatar) == "function" then
-                    AvatarComp:ChangeItemAvatar(1961025, true)
-                    LexusNotify("client")
+                if not slua.isValid(AvatarComp) then
+                    local VehicleCommon = playerVehicle:GetCommonComponent()
+                    if slua.isValid(VehicleCommon) and type(VehicleCommon.GetAvatarComponent) == "function" then
+                        AvatarComp = VehicleCommon:GetAvatarComponent()
+                    end
+                end
 
-                    if type(uPlayerController.ServerChangeVehicleAvatar) == "function" then
-                        uPlayerController:ServerChangeVehicleAvatar(1961025)
-                    end
+                if slua.isValid(AvatarComp) and type(AvatarComp.ChangeItemAvatar) == "function" then
+                    AvatarComp:ChangeItemAvatar(1961020, true)
 
-                    LexusNotify("Đã áp dụng skin siêu xe!")
-                    LastProcessedVehicle = playerVehicle
-                end
-            end
-        else
-            LastProcessedVehicle = nil
-        end
+                    if type(uPlayerController.ServerChangeVehicleAvatar) == "function" then
+                        uPlayerController:ServerChangeVehicleAvatar(1961020)
+                    end
 
-        -- 3. QUÉT ĐỊCH (ESP RADAR)
-        for _, enemy in pairs(_G.LexusEnemyCache) do
-            if slua.isValid(enemy) and enemy.ActiveForceMark and sMark then
-                InGameMarkTools.HideMapMark(enemy.ActiveForceMark)
-                enemy.ActiveForceMark = nil
-            end
-        end
+                    LexusNotify("Đã áp dụng skin siêu xe!")
+                    LastProcessedVehicle = playerVehicle
+                end
+            end
+        else
+            LastProcessedVehicle = nil
+        end
 
-        _G.LexusEnemyCache = {}
-        if CharacterClass then
-            local outActors = slua.Array(UEnums.EPropertyClass.Object, import("/Script/Engine.Actor"))
-            UGameplayStatics.GetAllActorsOfClass(uPlayerController, CharacterClass, outActors)
-            local myKey = uPlayerCharacter.PlayerKey
+        -- 3. QUÉT ĐỊCH (ESP RADAR)
+        for _, enemy in pairs(_G.LexusEnemyCache) do
+            if slua.isValid(enemy) and enemy.ActiveForceMark and sMark then
+                InGameMarkTools.HideMapMark(enemy.ActiveForceMark)
+                enemy.ActiveForceMark = nil
+            end
+        end
 
-            for i = 0, outActors:Num() - 1 do
-                local enemy = outActors:Get(i)
-                if slua.isValid(enemy) and enemy.PlayerKey ~= myKey and enemy.TeamID ~= uPlayerCharacter.TeamID then
-                    table.insert(_G.LexusEnemyCache, enemy)
-                    if sMark and type(InGameMarkTools.ClientAddMapMark) == "function" then
-                        local loc = type(enemy.GetHeadLocation) == "function" and enemy:GetHeadLocation(false) or enemy:K2_GetActorLocation()
-                        enemy.ActiveForceMark = InGameMarkTools.ClientAddMapMark(1003, loc, 0, "", i, nil)
-                    end
-                end
-            end
-        end
-    end
+        _G.LexusEnemyCache = {}
+        if CharacterClass then
+            local outActors = slua.Array(UEnums.EPropertyClass.Object, import("/Script/Engine.Actor"))
+            UGameplayStatics.GetAllActorsOfClass(uPlayerController, CharacterClass, outActors)
+            local myKey = uPlayerCharacter.PlayerKey
 
-    -- [LUỒNG NHANH] Cập nhật vị trí Radar (0.03s)
-    for _, enemy in pairs(_G.LexusEnemyCache) do
-        if slua.isValid(enemy) then
-            local isAlive = type(enemy.IsAlive) == "function" and enemy:IsAlive() or true
-            if isAlive and enemy.ActiveForceMark and sMark then
-                local headLoc = type(enemy.GetHeadLocation) == "function" and enemy:GetHeadLocation(false) or enemy:K2_GetActorLocation()
-                InGameMarkTools.UpdateMapMarkLocation(enemy.ActiveForceMark, headLoc)
-            end
-        end
-    end
+            for i = 0, outActors:Num() - 1 do
+                local enemy = outActors:Get(i)
+                if slua.isValid(enemy) and enemy.PlayerKey ~= myKey and enemy.TeamID ~= uPlayerCharacter.TeamID then
+                    table.insert(_G.LexusEnemyCache, enemy)
+                    if sMark and type(InGameMarkTools.ClientAddMapMark) == "function" then
+                        local loc = type(enemy.GetHeadLocation) == "function" and enemy:GetHeadLocation(false) or enemy:K2_GetActorLocation()
+                        enemy.ActiveForceMark = InGameMarkTools.ClientAddMapMark(1003, loc, 0, "", i, nil)
+                    end
+                end
+            end
+        end
+    end
+
+    -- [LUỒNG NHANH] Cập nhật vị trí Radar (0.03s)
+    for _, enemy in pairs(_G.LexusEnemyCache) do
+        if slua.isValid(enemy) then
+            local isAlive = type(enemy.IsAlive) == "function" and enemy:IsAlive() or true
+            if isAlive and enemy.ActiveForceMark and sMark then
+                local headLoc = type(enemy.GetHeadLocation) == "function" and enemy:GetHeadLocation(false) or enemy:K2_GetActorLocation()
+                InGameMarkTools.UpdateMapMarkLocation(enemy.ActiveForceMark, headLoc)
+            end
+        end
+    end
 end
 
 -- ==========================================
@@ -162,18 +177,18 @@ end
 -- ==========================================
 
 if _G.LexusLoopRunning == nil then
-    _G.LexusLoopRunning = true
+    _G.LexusLoopRunning = true
 
-    local function StartFastLoop()
-        pcall(LexusMainLoop)
-        local s, time_ticker = pcall(require, "common.time_ticker")
-        if s and time_ticker and time_ticker.AddTimerOnce then
-            time_ticker.AddTimerOnce(0.03, StartFastLoop)
-        end
-    end
+    local function StartFastLoop()
+        pcall(LexusMainLoop)
+        local s, time_ticker = pcall(require, "common.time_ticker")
+        if s and time_ticker and time_ticker.AddTimerOnce then
+            time_ticker.AddTimerOnce(0.03, StartFastLoop)
+        end
+    end
 
-    StartFastLoop()
-    _G.LexusNotify("Hệ thống Lexus Mod đã kích hoạt!")
+    StartFastLoop()
+    _G.LexusNotify("Hệ thống Lexus Mod đã kích hoạt!")
 end
 
 
@@ -184,16 +199,16 @@ end
 local LexusVehicleClass = {}
 
 function LexusVehicleClass:ChangeSkin(VH_SkinID)
-    local CurrentVehicle = self:GetOwner()
-    if slua.isValid(CurrentVehicle) then
-        local VehicleCommon = self:GetCommonComponent()
-        if slua.isValid(VehicleCommon) then
-            local AvatarComponent = VehicleCommon:GetAvatarComponent()
-            if slua.isValid(AvatarComponent) then
-                AvatarComponent:ChangeItemAvatar(VH_SkinID, true)
-            end
-        end
-    end
+    local CurrentVehicle = self:GetOwner()
+    if slua.isValid(CurrentVehicle) then
+        local VehicleCommon = self:GetCommonComponent()
+        if slua.isValid(VehicleCommon) then
+            local AvatarComponent = VehicleCommon:GetAvatarComponent()
+            if slua.isValid(AvatarComponent) then
+                AvatarComponent:ChangeItemAvatar(VH_SkinID, true)
+            end
+        end
+    end
 end
 
 local class = require("class")
